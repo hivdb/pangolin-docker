ad0fe646de12abcdbd934f9255fa9e2df93399c1
Philip Tzou <philip.npc@gmail.com>
2021-04-24 18:26:22 -0700

    Auto-create local/ folder

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ aws lambda update-function-code \
     --function-name pangolin-runner \
     --image-uri 931437602538.dkr.ecr.us-west-2.amazonaws.com/hivdb/pangolin-lambda:$LATEST_VERSION
 
+mkdir -p local/
 docker run -i --rm --volume ~/.aws:/root/.aws:ro --entrypoint python3 hivdb/pangolin-lambda:latest > local/latest_version <<EOF
 from pangolin import __version__ as pangolin_version
 from pangoLEARN import __version__ as pangoLEARN_version
