aa05d49f67755f910093e067e85deec8c6e062aa
Philip Tzou <philip.npc@gmail.com>
2021-10-13 15:48:05 -0700

    It's buggy for short sequence

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def main(event, context):
     proc = subprocess.run(
         ['/var/lang/bin/pangolin',
          '/tmp/input.fasta',
-         '--min-length', '3000',  # allow Spike-only sequence
          '-o', '/tmp',
          '--outfile', 'lineage-report.csv'],
         capture_output=True,
